6a30dac056c8cbd7d9b5572baccf3be1f2388831
youssefatti <youssefattia@gmail.com>
2018-03-12 10:40:25 +0100

    last push for this branch

--- src/screens/Items.js
+++ src/screens/Items.js
@@ -1,11 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+// import AppStyle from "../../AppStyle";
+// const styles = StyleSheet.create(AppStyle);
 
 export default class Items extends React.Component {
   render() {
+    let arrItems = [];
+    // on boucle sur le tableau items
+    for (let i = 0; i < this.props.idItem.length; i++) {
+      // on vérifie la correspondance entre l'id de la catégorie et la category_id de l'item et on récupere les infos
+      if (this.props.idCat.id === this.props.idItem[i].category_id) {
+        arrItems.push(
+          <View
+            key={i}
+            style={{
+              width: 300,
+              marginBottom: 10
+            }}
+          >
+            <Text style={styles.strong}>{this.props.idItem[i].name}</Text>
+            {this.props.idItem[i].description ? (
+              <Text>{this.props.idItem[i].description}</Text>
+            ) : null}
+            <Text style={[styles.strong, styles.text]}>
+              {this.props.idItem[i].price}
+            </Text>
+          </View>
+        );
+      }
+    }
     return (
       <View>
-        <Text>ok</Text>
+        <Text>{arrItems}</Text>
       </View>
     );
   }
