d0064330aa8989d9590c015cdb7dc2dd29eb78f0
youssefatti <youssefattia@gmail.com>
2018-03-12 10:06:11 +0100

    request for menu added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import axios from "axios";
+
+import geo from "geo-hash";
+
 import {
   StyleSheet,
   ScrollView,
@@ -14,7 +17,74 @@ export default class Home extends React.Component {
     header: null
   };
 
+  state = {
+    latitude: null,
+    longitude: null,
+    error: null,
+    geoloc: null
+  };
+
+  componentDidMount() {
+    console.log("did mount ");
+    const lat = 48.85674155317247;
+    const lon = 2.3544311709702015;
+
+    let geohash = geo.encode(lat, lon); //"u09tvw47nhxp";
+    this.setState({
+      geoloc: geohash
+    });
+    console.log("geo hash : ", geohash);
+
+    this.watchId = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        });
+      },
+      error => this.setState({ error: error.message }),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 10
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
   render() {
+    //navigator.geolocation.requestAuthorization();
+    // var options = {
+    //   enableHighAccuracy: false,
+    //   timeout: 10000,
+    //   maximumAge: 0
+    // };
+
+    // function success(pos) {
+    //   console.log("pos : ", pos);
+    //   var crd = pos.coords;
+
+    //   console.log("Your current position is:");
+    //   console.log(`Latitude : ${crd.latitude}`);
+    //   console.log(`Longitude: ${crd.longitude}`);
+    //   console.log(`More or less ${crd.accuracy} meters.`);
+    //   console.log("watchID : ", watchID);
+    // }
+
+    // function error(err) {
+    //   console.warn(`ERROR(${err.code}): ${err.message}`);
+    // }
+
+    // let watchID = navigator.geolocation.watchPosition(success, error, options);
+    // navigator.geolocation.clearWatch(watchID);
+
+    // navigator.geolocation.getCurrentPosition(success, error, options);
+
     const { navigate } = this.props.navigation;
     return (
       <ScrollView style={[styles.container, styles.style, styles.bgColorHome]}>
@@ -22,7 +92,8 @@ export default class Home extends React.Component {
           <TouchableOpacity
             onPress={() =>
               navigate("Restaurants", {
-                name: "Restaurant"
+                name: "Restaurant",
+                geoloc: this.state.geoloc
               })
             }
           >
@@ -30,6 +101,17 @@ export default class Home extends React.Component {
               <Text style={styles.logoText}>FoodPicking</Text>
             </View>
           </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flexGrow: 1,
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Text>Latitude: {this.state.latitude}</Text>
+          <Text>Longitude: {this.state.longitude}</Text>
+          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         </View>
       </ScrollView>
     );
